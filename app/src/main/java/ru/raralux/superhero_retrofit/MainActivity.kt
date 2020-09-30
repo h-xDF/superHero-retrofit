31e3df6eaf111134b68e9ebae8a6f936a71961c3
Yaroslav Pereyaslov <y.pereyaslov@semrush.com>
2020-09-30 15:27:17 +0300

    added adapter, compatibility java8

--- app/src/main/java/ru/raralux/superhero_retrofit/MainActivity.kt
+++ app/src/main/java/ru/raralux/superhero_retrofit/MainActivity.kt
@@ -2,10 +2,51 @@ package ru.raralux.superhero_retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.raralux.superhero_retrofit.`interface`.RetrofitServices
+import ru.raralux.superhero_retrofit.adapter.MyMovieAdapter
+import ru.raralux.superhero_retrofit.common.Common
+import ru.raralux.superhero_retrofit.model.Movie
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var mService: RetrofitServices
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var adapter: MyMovieAdapter
+    lateinit var dialog: AlertDialog
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        mService = Common.retrofitServices
+        recyclerMovieList.setHasFixedSize(true)
+        layoutManager = LinearLayoutManager(this)
+        recyclerMovieList.layoutManager = layoutManager
+        
+        getAllMovieList()
+    }
+    
+    private fun getAllMovieList() {
+        
+        mService.getMovieList().enqueue(object : Callback<MutableList<Movie>> {
+            override fun onFailure(call: Call<MutableList<Movie>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(
+                call: Call<MutableList<Movie>>,
+                response: Response<MutableList<Movie>>
+            ) {
+                adapter= MyMovieAdapter(baseContext, response.body() as MutableList<Movie>)
+                adapter.notifyDataSetChanged()
+                recyclerMovieList.adapter = adapter
+            }
+        })
     }
 }
